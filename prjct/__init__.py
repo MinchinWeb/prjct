212845d117dc70a200a49795c10f1369256de217
MinchinWeb <w_minchin@hotmail.com>
2017-03-05 22:40:57 -0700

    bump version number

--- prjct/__init__.py
+++ prjct/__init__.py
@@ -7,15 +7,15 @@ prjct: Project Management for Living Life
 
 __title__ = 'prjct'
 __tagline__ = 'Project Management for Living Life'
-__version__ = '0.4.0+dev.20161230.0'
+__version__ = '0.5.0+dev.20170305.0'
 __author__ = 'William Minchin'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2013-16 William Minchin'
 __url__ = 'https://github.com/MinchinWeb/prjct'
 
 # import submodules
-from . import todo_export
 from . import config
-from . import sphinx
 from . import descriptions
 from . import multi_source
+from . import sphinx
+from . import todo_export
